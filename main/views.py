b1eba8447d022c415d5b20ab8ace9bdd8151dcfa
Andreevvs <34914911+Andreevvs@users.noreply.github.com>
2022-03-15 17:56:35 +0300

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import ListView, DetailView  # импортируем класс получения деталей объекта
+from .models import Post
+from datetime import datetime
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+
+    # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словаря и есть переменные, к которым мы сможем потом обратиться через шаблон
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now()  # добавим переменную текущей даты time_now
+        return context
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class NewsDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'newsdetail.html'  # название шаблона будет product.html
+    context_object_name = 'newsdetail'  # название объекта. в нём будет
+
+# Create your views here.
